b01fd6cfe0e177f8cf67d4ca61f1c61abf918dae
Chris felicien <f.ilungakalenga@gmail.com>
2026-06-17 16:50:08 +0200

    feat(auth): Implement Refresh token schema and implement Refresh token test

---
+++ server/src/models/RefreshToken.model.ts
@@ -0,0 +1,43 @@
+import IRefreshToken from '@/interfaces/IRefreshToken';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+
+const RefreshTokenSchema = new mongoose.Schema<IRefreshToken>(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user id'],
+    },
+    tokenHash: {
+      type: String,
+      required: [true, 'Please provide a refresh token'],
+    },
+    expiresAt: {
+      type: Date,
+      required: [true, 'Please provide the expired token date'],
+    },
+  },
+  { timestamps: true },
+);
+
+RefreshTokenSchema.index({ userId: 1, tokenHash: 1 }, { unique: true });
+
+RefreshTokenSchema.pre('save', function () {
+  if (!this.isModified('tokenHash')) return;
+  this.tokenHash = crypto
+    .createHash('sha256')
+    .update(this.tokenHash)
+    .digest('hex');
+
+  return;
+});
+
+RefreshTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+const RefreshToken = mongoose.model<IRefreshToken>(
+  'RefreshToken',
+  RefreshTokenSchema,
+);
+
+export default RefreshToken;
