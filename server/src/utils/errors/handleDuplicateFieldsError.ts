e81e6a56a5a420827b97ede46104f054e4fe4965
Chris felicien <f.ilungakalenga@gmail.com>
2026-06-29 14:55:58 +0200

    Fix: Change message to duplicate field '

--- server/src/utils/errors/handleDuplicateFieldsError.ts
+++ server/src/utils/errors/handleDuplicateFieldsError.ts
@@ -2,11 +2,9 @@ import AppError from '@/utils/AppError';
 
 const handleDuplicateFieldsError = (error: any) => {
   const field = Object.keys(error.keyValue)[0];
-  let value;
-  if (field) value = error.keyValue[field];
 
   return new AppError(
-    `${field} "${value}" already exists. Please use another value.`,
+    `${field} already exists. Please use another value.`,
     409,
   );
 };
