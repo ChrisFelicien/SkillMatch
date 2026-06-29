ff0c0f6be8369ca33c0973a1e3f92e1007b00d1a
Chris felicien <f.ilungakalenga@gmail.com>
2026-06-29 14:56:59 +0200

    add jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,14 +1,14 @@
-const { createDefaultPreset } = require("ts-jest");
+const { createDefaultPreset } = require('ts-jest');
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'node',
   transform: {
-    ...tsJestTransformCfg
+    ...tsJestTransformCfg,
   },
-  preset: "ts-jest",
-  roots: ["<rootDir>/src"],
-  moduleNameMapper: { "^@/(.*)$": "<rootDir>/src/$1" }
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
 };
