0577212d2b0d115a52e78a5419e60cd5e6b5d96a
Chris felicien <f.ilungakalenga@gmail.com>
2026-06-30 15:27:40 +0200

    feat/job: Implement job router

---
+++ server/src/routes/v1/Job.route.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import JobController from '@/controllers/Job.controller';
+import AuthMiddleware from '@/middlewares/Auth.middleware';
+import { UserRoles } from '@/interfaces/IUser';
+import catchAsyncError from '@/utils/catchAsyncError';
+
+const router: Router = Router();
+
+router
+  .route('/')
+  .get(JobController.getAllJobs)
+  .post(
+    AuthMiddleware.protect,
+    AuthMiddleware.restrictTo([UserRoles.CLIENT, UserRoles.ADMIN]),
+    // will add zod
+    catchAsyncError(JobController.createJob),
+  );
+router
+  .route('/:jobId')
+  .get(catchAsyncError(JobController.getSingleJob))
+  .delete(
+    AuthMiddleware.protect,
+    AuthMiddleware.restrictTo([UserRoles.CLIENT, UserRoles.ADMIN]),
+    // will add zod
+    catchAsyncError(JobController.deleteJob),
+  )
+  .patch(
+    AuthMiddleware.protect,
+    AuthMiddleware.restrictTo([UserRoles.CLIENT, UserRoles.ADMIN]),
+    // will add zod
+    catchAsyncError(JobController.updateJob),
+  );
+
+export default router;
