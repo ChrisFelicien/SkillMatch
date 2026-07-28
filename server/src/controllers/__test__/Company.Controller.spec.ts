152c14d2e68af5a90a42805bc0571f168d13404e
Chris felicien <f.ilungakalenga@gmail.com>
2026-07-28 15:57:25 +0200

    feat(job): Implement test to approve or reject become client request

--- server/src/controllers/__test__/Company.Controller.spec.ts
+++ server/src/controllers/__test__/Company.Controller.spec.ts
@@ -6,6 +6,8 @@ import { connect, closeDatabase, clearDatabase } from '@/test/dbHandler';
 import User from '@/models/User.model';
 import { UserRoles } from '@/interfaces/IUser';
 import { Types } from 'mongoose';
+import Company from '@/models/Company.model';
+import { CompanyStatus } from '@/interfaces/ICompany';
 
 const agent = request.agent(app);
 
@@ -307,5 +309,312 @@ describe('Test get single request', () => {
     const response = await agent.get(`/api/v1/companies/invalid-mongo-id`);
 
     expect(response.statusCode).toBe(400);
+  });
+});
+
+describe('Test to approve request', () => {
+  it('Should approve request', async () => {
+    const userData = userFactory();
+    const companyData = companyFactory();
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const result1 = await agent
+      .post('/api/v1/companies')
+      .send({ ...companyData, owner: result.body.user._id });
+
+    const result2 = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userData.password });
+
+    await User.findByIdAndUpdate(
+      result2.body.user._id,
+      { role: UserRoles.ADMIN },
+      { new: true },
+    );
+
+    const id = result1.body.request._id;
+
+    const response = await agent.patch(`/api/v1/companies/${id}/approve`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.request.status).toBe(CompanyStatus.APPROVED);
+  });
+
+  it('Should fail when user is not authenticated', async () => {
+    const id = new Types.ObjectId();
+    const response = await request(app).patch(
+      `/api/v1/companies/${id}/approve`,
+    );
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  it('Should fail when user is not admin', async () => {
+    const userData = userFactory();
+    const id = new Types.ObjectId();
+
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const response = await agent.patch(`/api/v1/companies/${id}/approve`);
+
+    expect(response.statusCode).toBe(403);
+  });
+
+  it('SHould fail when request doest not exist', async () => {
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const userData = userFactory();
+    const result2 = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userData.password });
+
+    await User.findByIdAndUpdate(
+      result2.body.user._id,
+      { role: UserRoles.ADMIN },
+      { new: true },
+    );
+
+    const id = new Types.ObjectId();
+
+    const response = await agent.patch(`/api/v1/companies/${id}/approve`);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe('No request found with this id');
+  });
+
+  it('Should fail if the request has been already approved', async () => {
+    const userData = userFactory();
+    const companyData = companyFactory();
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const result1 = await agent
+      .post('/api/v1/companies')
+      .send({ ...companyData, owner: result.body.user._id });
+
+    const result2 = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userData.password });
+
+    await User.findByIdAndUpdate(
+      result2.body.user._id,
+      { role: UserRoles.ADMIN },
+      { new: true },
+    );
+
+    const id = result1.body.request._id;
+    await agent.patch(`/api/v1/companies/${id}/approve`);
+    const response = await agent.patch(`/api/v1/companies/${id}/approve`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe('This request has been processed');
+  });
+
+  it('Should fail if the request has been already rejected', async () => {
+    const userData = userFactory();
+    const companyData = companyFactory();
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const result1 = await agent
+      .post('/api/v1/companies')
+      .send({ ...companyData, owner: result.body.user._id });
+
+    await Company.findByIdAndUpdate(result1.body.request._id, {
+      status: CompanyStatus.REJECTED,
+    });
+
+    const result2 = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userData.password });
+
+    await User.findByIdAndUpdate(
+      result2.body.user._id,
+      { role: UserRoles.ADMIN },
+      { new: true },
+    );
+
+    const id = result1.body.request._id;
+
+    const response = await agent.patch(`/api/v1/companies/${id}/approve`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe('This request has been processed');
+  });
+
+  it('It should fail when request owner is no longer exist', async () => {
+    const userData = userFactory();
+    const companyData = companyFactory();
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const result1 = await agent
+      .post('/api/v1/companies')
+      .send({ ...companyData, owner: result.body.user._id });
+
+    await User.findByIdAndDelete(result.body.user._id);
+
+    const result2 = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userData.password });
+
+    await User.findByIdAndUpdate(
+      result2.body.user._id,
+      { role: UserRoles.ADMIN },
+      { new: true },
+    );
+
+    const id = result1.body.request._id;
+
+    const response = await agent.patch(`/api/v1/companies/${id}/approve`);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe('No user find.');
+  });
+});
+
+describe('Test to reject client request', () => {
+  it('Should reject client request', async () => {
+    const userData = userFactory();
+    const companyData = companyFactory();
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const result1 = await agent
+      .post('/api/v1/companies')
+      .send({ ...companyData, owner: result.body.user._id });
+
+    const result2 = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userData.password });
+
+    await User.findByIdAndUpdate(
+      result2.body.user._id,
+      { role: UserRoles.ADMIN },
+      { new: true },
+    );
+
+    const id = result1.body.request._id;
+
+    const response = await agent.patch(`/api/v1/companies/${id}/reject`);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.request.status).toBe(CompanyStatus.REJECTED);
+  });
+
+  it('Should fail if user is not a admin', async () => {
+    const userData = userFactory();
+    const id = new Types.ObjectId();
+    //  const companyData = companyFactory();
+    //  const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const response = await agent.patch(`/api/v1/companies/${id}/reject`);
+
+    expect(response.statusCode).toBe(403);
+    expect(response.body.message).toBe(
+      'You are not allowed to perform this action',
+    );
+  });
+
+  it('Should fail when the request is not exist', async () => {
+    const id = new Types.ObjectId();
+
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userAdmin.password });
+
+    await User.findByIdAndUpdate(result.body.user._id, {
+      role: UserRoles.ADMIN,
+    });
+
+    const response = await agent.patch(`/api/v1/companies/${id}/reject`);
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe('No request found with this id');
+  });
+
+  it('Should fail when request is already rejected', async () => {
+    const userData = userFactory();
+    const companyData = companyFactory();
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const result1 = await agent
+      .post('/api/v1/companies')
+      .send({ ...companyData, owner: result.body.user._id });
+
+    await Company.findByIdAndUpdate(result1.body.request._id, {
+      status: CompanyStatus.REJECTED,
+    });
+
+    const result2 = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userData.password });
+
+    await User.findByIdAndUpdate(
+      result2.body.user._id,
+      { role: UserRoles.ADMIN },
+      { new: true },
+    );
+
+    const id = result1.body.request._id;
+
+    const response = await agent.patch(`/api/v1/companies/${id}/reject`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe('This request has been processed');
+  });
+
+  it('Should fail when the request has already been approved', async () => {
+    const userData = userFactory();
+    const companyData = companyFactory();
+    const userAdmin = userFactory({ email: 'admin@admin.com' });
+    const result = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userData, confirmPassword: userData.password });
+
+    const result1 = await agent
+      .post('/api/v1/companies')
+      .send({ ...companyData, owner: result.body.user._id });
+
+    await Company.findByIdAndUpdate(result1.body.request._id, {
+      status: CompanyStatus.APPROVED,
+    });
+
+    const result2 = await agent
+      .post('/api/v1/auth/register')
+      .send({ ...userAdmin, confirmPassword: userData.password });
+
+    await User.findByIdAndUpdate(
+      result2.body.user._id,
+      { role: UserRoles.ADMIN },
+      { new: true },
+    );
+
+    const id = result1.body.request._id;
+
+    const response = await agent.patch(`/api/v1/companies/${id}/reject`);
+
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toBe('This request has been processed');
   });
 });
