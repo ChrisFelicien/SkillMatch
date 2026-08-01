fcec616a84247ec52bed06011c3baf16cfcb6390
Chris felicien <f.ilungakalenga@gmail.com>
2026-08-01 14:45:00 +0200

    feat(application): Impliment the application model with schemas property

---
+++ server/src/models/Application.model.ts
@@ -0,0 +1,44 @@
+import IApplication, { ApplicationStatus } from '@/interfaces/Application';
+import mongoose from 'mongoose';
+
+const ApplicationSchema = new mongoose.Schema<IApplication>(
+  {
+    freelancer: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide the freelancer id'],
+    },
+    job: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Job',
+      required: [true, 'Please provide the job id'],
+    },
+    company: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Company',
+      required: [true, 'Please provide company id'],
+    },
+    coverLetter: {
+      type: String,
+      required: [true, 'Please provide the cover letter'],
+    },
+    resume: {
+      type: String,
+      required: [true, 'Please provide the resume'],
+    },
+    status: {
+      type: String,
+      enum: Object.values(ApplicationStatus),
+      default: ApplicationStatus.PENDING,
+    },
+  },
+  { timestamps: true },
+);
+
+ApplicationSchema.index({ freelancer: 1, index: 1 }, { unique: true });
+
+const Application = mongoose.model<IApplication>(
+  'Application',
+  ApplicationSchema,
+);
+export default Application;
