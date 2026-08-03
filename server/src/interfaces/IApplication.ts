22924b1c1485c903848abd9390dfcb47f54bf36b
Chris felicien <f.ilungakalenga@gmail.com>
2026-08-03 14:06:23 +0200

    feat(application): Rename application interface

---
+++ server/src/interfaces/IApplication.ts
@@ -0,0 +1,31 @@
+import { Types } from 'mongoose';
+
+export enum ApplicationStatus {
+  PENDING = 'pending',
+  REVIEWING = 'reviewing',
+  INTERVIEW = 'interview',
+  ACCEPTED = 'accepted',
+  REJECTED = 'rejected',
+}
+
+interface IApplication {
+  freelancer: Types.ObjectId;
+  job: Types.ObjectId;
+  company: Types.ObjectId;
+  coverLetter: string;
+  resume: string;
+  status: ApplicationStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface ApplyToJobDTO {
+  freelancer: Types.ObjectId;
+  job: Types.ObjectId;
+  company: Types.ObjectId;
+  coverLetter: string;
+  resume: string;
+  status?: ApplicationStatus;
+}
+
+export default IApplication;
